34616d187c8bbb794d819d4a2de1eab498a7d55d
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2021-08-07 18:40:53 -0400

    fix(composable): Fix story loading on client side

--- src/composables/useStory.ts
+++ src/composables/useStory.ts
@@ -1,5 +1,5 @@
 import { ref, computed, onMounted } from 'vue-demi';
-import { onSSR } from '@vue-storefront/core';
+import { onSSR, sharedRef } from '@vue-storefront/core';
 import { useStoryblokContent } from './useStoryblokContent';
 import { storyblokBridge } from '../helpers/storyblokBridge';
 
@@ -8,18 +8,29 @@ const useStory = (
   { skipLoading, prefetchedStory } = { skipLoading: false, prefetchedStory: null }
 ) => {
   const { loading, content, error, search } = useStoryblokContent(slug, prefetchedStory);
+  const storyLoaded = sharedRef(false, `${slug}-storyLoaded`);
   const changedStory = ref(null);
   const story = computed(() => changedStory.value || content.value);
   const storyContent = computed(() => story.value?.content || null);
   const loadStory = async () => search({ slug });
 
-  if (!skipLoading && !prefetchedStory) {
+  if (!storyLoaded.value) {
+    storyLoaded.value = skipLoading || Boolean(prefetchedStory);
+  }
+
+  if (!storyLoaded.value) {
     onSSR(async () => {
+      storyLoaded.value = true;
       await loadStory();
     });
   }
 
   onMounted(async () => {
+    if (!storyLoaded.value) {
+      storyLoaded.value = true;
+      await loadStory();
+    }
+
     /_storyblok/.test(window.location.search) &&
       storyblokBridge(
         content.value,
