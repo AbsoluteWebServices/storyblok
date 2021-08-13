a1667e4effba335331fb73cf52fd2e2ef353001e
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2021-08-13 14:33:41 -0400

    feat(cache): Add cache tags

--- src/index.server.ts
+++ src/index.server.ts
@@ -1,4 +1,4 @@
-import { apiClientFactory } from '@vue-storefront/core'
+import { ApiClientExtension, apiClientFactory } from '@vue-storefront/core'
 import { ApiContext, ContentSearchParams } from './types'
 import StoryblokClient from 'storyblok-js-client'
 
@@ -14,11 +14,27 @@ const setup = ({ token, cacheProvider }: ContentSearchParams): ApiContext => {
   }
 }
 
+
+const cacheExtension: ApiClientExtension = {
+  name: 'cacheExtension',
+  hooks: (req, res) => ({
+    afterCall: ({ configuration, response }) => {
+      if (response && response.id) {
+        const cacheTagsHeaderName = configuration.headers?.cacheTagsHeaderName || 'x-cache-tags';
+
+        res.header(cacheTagsHeaderName, 'sb_' + response.id);
+      }
+      return response;
+    }
+  }),
+};
+
 const { createApiClient } = apiClientFactory({
   onCreate: setup,
   api: {
     getContent,
   },
+  extensions: [cacheExtension],
 } as any)
 
 export { createApiClient }
