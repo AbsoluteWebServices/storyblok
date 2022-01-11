eb4dc22ccfbee99378b0f81858b60dad2bf5daae
Georgiy Slobodenyuk <georgiy@absoluteweb.com>
2022-01-11 17:44:55 -0500

    Avoid loading storyblok if not in a preview

--- src/helpers/storyblokBridge.js
+++ src/helpers/storyblokBridge.js
@@ -3,21 +3,37 @@ export const storyblokBridge = (
   events = ['change', 'input'],
   relations = [],
   callback,
+  bridgeUrl = '',
 ) => {
   if (window) {
-    // eslint-disable-next-line
-    const instance = new StoryblokBridge({
-      resolveRelations: relations,
-    })
-    instance.on(events, (payload) => {
-      if (!payload.story || payload.story.id !== content._meta.id) {
-        return
-      }
-
-      callback({
-        ...payload.story.content,
-        _meta: payload.story,
+    const loadStoryblok = () => {
+      const instance = new StoryblokBridge({
+        resolveRelations: relations,
       })
-    })
+      instance.on(events, (payload) => {
+        if (!payload.story || payload.story.id !== content._meta.id) {
+          return
+        }
+
+        callback({
+          ...payload.story.content,
+          _meta: payload.story,
+        })
+      })
+    };
+    // eslint-disable-next-line
+    if (!window.StoryblokBridge) {
+      const existingScript = document.getElementById('storyblok_script');
+      if (!existingScript) {
+        const js = document.createElement('script');
+        js.id = 'storyblok_script';
+        js.async = true;
+        js.src = bridgeUrl;
+        js.onload = loadStoryblok;
+        document.body.appendChild(js);
+      }
+    } else {
+      loadStoryblok();
+    }
   }
 }
